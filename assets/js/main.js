61aff12b0031cdaff899fe4839d713e8db3bfe15
CloudByteSolutions <adam.alaraj@cloudbytesolutions.com>
2025-03-05 19:38:45 -0600

    93

--- assets/js/main.js
+++ assets/js/main.js
@@ -522,7 +522,7 @@
 			$('body').html(
 			  '<div style="width:100%; height:800px;">' +
 				'<iframe src="https://booking.moego.pet/ol/GroomandZoom121847/landing?utm_medium=embed" ' +
-						'style="width:100%; height:800px; border:0;" ' +
+						'style="width:100%; height:93%; border:0;" ' +
 						'title="Online booking" scrolling="no"></iframe>' +
 			  '</div>'
 			).fadeIn(500); // Fade in the new content over 500ms
